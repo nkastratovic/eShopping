a0290db05fc725ada7083fb7f2ed15e3175ef6b8
nkastratovic <nkastratovic@gmail.com>
2024-02-13 13:11:49 +0100

    Catalog Implementing Lazy Mapper.

--- src/Services/Catalog/Catalog.Application/Handlers/GetAllBrandsHandler.cs
+++ src/Services/Catalog/Catalog.Application/Handlers/GetAllBrandsHandler.cs
@@ -11,17 +11,15 @@ namespace Catalog.Application.Handlers;
 public class GetAllBrandsHandler : IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
 {
     private readonly IBrandRepository _brandRepository;
-  private readonly IMapper _mapper;
-    
-    public GetAllBrandsHandler(IBrandRepository brandRepository, IMapper mapper)
+
+    public GetAllBrandsHandler(IBrandRepository brandRepository)
     {
         _brandRepository = brandRepository;
-        _mapper = mapper;
-  }
+    }
     public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
     {
         var brandList = await _brandRepository.GetAllBrands();
-        var brandResponseList = _mapper.Map<IList<ProductBrand>, IList<BrandResponse>>(brandList.ToList());
+        var brandResponseList = ProductMapper.Mapper.Map<IList<ProductBrand>, IList<BrandResponse>>(brandList.ToList());
         return brandResponseList;
     }
 }
