8962ff8a7268a0825ab0bd78590dae8df7c5ad93
nkastratovic <nkastratovic@gmail.com>
2024-02-14 08:49:33 +0100

    Ger Product by Id controller method.

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -1,9 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Net;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Catalog.API
+namespace Catalog.API.Controllers;
+
+public class CatalogController : ApiController
 {
-    public class CatalogController : ControllerBase
-    {
-    }
+  private readonly IMediator _mediator;
+
+  public CatalogController(IMediator mediator)
+  {
+    _mediator = mediator;
+  }
+
+  [HttpGet]
+  [Route("[action]/{id}", Name = "GetProductById")]
+  [ProducesResponseType(typeof(ProductResponse), (int)HttpStatusCode.OK)]
+  public async Task<ActionResult<ProductResponse>> GetProductById(string id)
+  {
+    var query = new GetProductByIdQuery(id);
+    var result = await _mediator.Send(query);
+    return Ok(result);
+  }
 }
