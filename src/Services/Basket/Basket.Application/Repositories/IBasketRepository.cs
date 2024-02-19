2c76cc8687306ce4f15c3be2cba92845c0274315
nkastratovic <nkastratovic@gmail.com>
2024-02-19 11:49:24 +0100

    Basket Repository implementation.

---
+++ src/Services/Basket/Basket.Application/Repositories/IBasketRepository.cs
@@ -0,0 +1,10 @@
+﻿using Basket.Core.Entities;
+
+namespace Basket.Application.Repositories;
+
+public interface IBasketRepository
+{
+    Task<ShoppingCart> GetBasket(string userName);
+    Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart);
+    Task DeleteBasket(string userName);
+}
