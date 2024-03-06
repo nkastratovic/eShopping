9124bd99236aaff798911ba13c021412bb8f0b10
nkastratovic <nkastratovic@gmail.com>
2024-03-06 16:07:53 +0100

    Validation Exception.

--- src/Services/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
+++ src/Services/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -1,9 +1,9 @@
-﻿namespace Ordering.Application.Extensions;
+﻿namespace Ordering.Application.Exceptions;
 
 public class OrderNotFoundException : ApplicationException
 {
     public OrderNotFoundException(string name, object key) : base($"Entity {name} - {key} is not found.")
     {
-        
+
     }
 }
