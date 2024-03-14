ef1e2f001f7a41d4023c4ba5b4c7700f9f795a31
nkastratovic <nkastratovic@gmail.com>
2024-03-14 12:52:54 +0100

    Startup Changes for Mass Transit.

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -5,6 +5,7 @@ using Basket.Core.Repositories;
 using Basket.Infrastructure.Repositories;
 using Discount.Grpc.Protos;
 using HealthChecks.UI.Client;
+using MassTransit;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.OpenApi.Models;
@@ -13,57 +14,67 @@ namespace Basket.API;
 
 public class Startup
 {
-  public IConfiguration Configuration;
+    public IConfiguration Configuration;
 
-  public Startup(IConfiguration configuration)
-  {
-    Configuration = configuration;
-  }
-
-  public void ConfigureServices(IServiceCollection services)
-  {
-    services.AddControllers();
-    services.AddApiVersioning();
-    //Redis Settings
-    services.AddStackExchangeRedisCache(options =>
+    public Startup(IConfiguration configuration)
     {
-      options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
-    });
-    services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateShoppingCartCommandHandler).GetTypeInfo().Assembly));
-    services.AddScoped<IBasketRepository, BasketRepository>();
-    services.AddAutoMapper(typeof(Startup));
-    services.AddScoped<DiscountGrpcService>();
-    services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
-        (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
-
-    services.AddSwaggerGen(c =>
-    {
-      c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
-    });
-    services.AddHealthChecks()
-      .AddRedis(Configuration["CacheSettings:ConnectionString"], "Redis Health", HealthStatus.Degraded);
-  }
-
-  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-  {
-    if (env.IsDevelopment())
-    {
-      app.UseDeveloperExceptionPage();
-      app.UseSwagger();
-      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
+        Configuration = configuration;
     }
 
-    app.UseHttpsRedirection();
-    app.UseRouting();
-    app.UseAuthorization();
-    app.UseEndpoints(endpoints =>
+    public void ConfigureServices(IServiceCollection services)
     {
-      endpoints.MapControllers();
-      endpoints.MapHealthChecks("/health", new HealthCheckOptions
-      {
-        Predicate = _ => true,
-        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-      });
-    });
-  }
+        services.AddControllers();
+        services.AddApiVersioning();
+
+        // Redis Settings
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
+        });
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateShoppingCartCommandHandler).GetTypeInfo().Assembly));
+        services.AddScoped<IBasketRepository, BasketRepository>();
+        services.AddAutoMapper(typeof(Startup));
+        services.AddScoped<DiscountGrpcService>();
+        services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+            (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+        });
+        services.AddHealthChecks()
+          .AddRedis(Configuration["CacheSettings:ConnectionString"], "Redis Health", HealthStatus.Degraded);
+        services.AddMassTransit(config =>
+        {
+            config.UsingRabbitMq((ct, cfg) =>
+            {
+                cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+            });
+        });
+        services.AddMassTransitHostedService();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapHealthChecks("/health", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+        });
+    }
 }
