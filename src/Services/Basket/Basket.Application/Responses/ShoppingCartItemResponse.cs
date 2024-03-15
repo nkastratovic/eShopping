b13b16c900943b336990a51e2802d447b6ed0f25
nkastratovic <nkastratovic@gmail.com>
2024-03-15 08:13:05 +0100

    Add RabbitMQ.

--- src/Services/Basket/Basket.Application/Responses/ShoppingCartItemResponse.cs
+++ src/Services/Basket/Basket.Application/Responses/ShoppingCartItemResponse.cs
@@ -1,11 +1,12 @@
-﻿namespace Basket.Application.Responses
+﻿namespace Basket.Application.Responses;
+
+public class ShoppingCartItemResponse
 {
-    public class ShoppingCartItemResponse
-    {
-        public int Quantity { get; set; }
-        public string ImageFile { get; set; }
-        public decimal Price { get; set; }
-        public string ProductId { get; set; }
-        public string ProductName { get; set; }
-    }
+  public string ProductId { get; set; }
+  public string ProductName { get; set; }
+  public int Quantity { get; set; }
+  public decimal Price { get; set; }
+  public string ImageFile { get; set; }
+
+
 }
