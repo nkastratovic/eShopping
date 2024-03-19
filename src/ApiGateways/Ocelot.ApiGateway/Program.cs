ccb996b5f0971b638f9510216cba5a58bcf7647a
nkastratovic <nkastratovic@gmail.com>
2024-03-19 15:41:56 +0100

    Progrm file changes.

--- src/ApiGateways/Ocelot.ApiGateway/Program.cs
+++ src/ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -1,6 +1,23 @@
-﻿namespace Ocelot.ApiGateway;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Ocelot.ApiGateway;
 
 public class Program
 {
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
 
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((env, config) =>
+            {
+                config.AddJsonFile($"ocelot.{env.HostingEnvironment.EnvironmentName}.json", true, true);
+            }).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
